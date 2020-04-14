954b4d1bd08de6354f1f8b95ba1048318ff57c71
oti <otiext@gmail.com>
2020-04-14 20:40:31 +0900

    開発環境を追加した

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+const dayjs = require("dayjs");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/**/*.(png|jpg|svg|webp)");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+
+  eleventyConfig.addWatchTarget("./src/");
+
+  // filter
+  eleventyConfig.addNunjucksFilter("dateFormat", (v) => {
+    return dayjs(v).format("YYYY年MM月DD日 HH:mm");
+  });
+
+  // filter
+  eleventyConfig.addNunjucksFilter("dateISOFormat", (v) => {
+    return dayjs(v).format("YYYY-MM-DDTHH:mmZ");
+  });
+
+  return {
+    dir: { input: "src", output: "docs" },
+    passthroughFileCopy: true,
+    templateFormats: ["njk", "md", "html", "yml"],
+    htmlTemplateEngine: "njk",
+  };
+};
